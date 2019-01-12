1e161b7309a50c6b7519b72f3c19c1d275977539
jack353249002 <353249002@qq.com>
2019-01-12 11:33:30 +0800

    完成断点传输功能

--- OxygenBurst.php
+++ OxygenBurst.php
@@ -11,22 +11,24 @@
  */
 class OxygenBurst
 {
-    private $token='';//会话
+    private $token='';//文件会话(指纹)
     private $cacheUrl='';//会话缓存地址
     private $conformityUrl='';//整合后存放地址
     private $suffix=''; //存储的后缀名
     private $total=0;//总共的个数
-    private $successFileCount=0;//已经成功上传的个数
     private $nowFile=null;
-    private $jsonInfor='';
-    public function __construct($token,$cacheUrl,$conformityUrl='',$suffix='',$total=0,$successFileCount=0,$nowFile=null){
+    private $jsonInfor=null;
+    private $shardFingerprint='';//分片指纹
+    private $fragmentedIndexes=null;//分片索引
+    public function __construct($token,$shardFingerprint,$cacheUrl,$conformityUrl='',$suffix='',$total=0,$fragmentedIndexes=null,$nowFile=null){
         $this->token=$token;
         $this->cacheUrl=$cacheUrl;
         $this->conformityUrl=$conformityUrl;
         $this->suffix=$suffix;
         $this->total=$total;
-        $this->successFileCount=$successFileCount;
         $this->nowFile=$nowFile;
+        $this->shardFingerprint=$shardFingerprint;
+        $this->fragmentedIndexes=$fragmentedIndexes;
         if(!file_exists($this->cacheUrl.'/'.$this->token)) {
             self::createTokenDir();
             $json=self::createJsonConfig();
@@ -36,12 +38,19 @@ class OxygenBurst
             $this->jsonInfor=self::readJson($this->cacheUrl.'/'.$this->token.'/'.'config.json');
         }
     }
+    /*获取指定令牌下的配置信息*/
+    public function getConfigForToken(){
+        return $this->jsonInfor;
+    }
     /*上传文件*/
     public function uploadFile(){
-        $obj= new OxygenUpload($this->cacheUrl.'/'.$this->token,'','file','',true);
+        $obj= new OxygenUpload($this->cacheUrl.'/'.$this->token,'','file',$this->shardFingerprint,true);
         $obj->mandatorySuffixName='rep';
-        $imgUrl=$obj->createFileMd5Name()->runUpload($this->nowFile)->returnUrl();
-        $this->jsonInfor['nowFile'][]=$imgUrl;
+        $imgUrl=$obj->runUpload($this->nowFile)->returnUrl();
+        $fileObj['Fingerprint']=$this->shardFingerprint;
+        $fileObj['url']=$imgUrl;
+        $fileObj['fragmentedIndexes']=$this->fragmentedIndexes;
+        $this->jsonInfor['nowFile']["{$this->shardFingerprint}"]=$fileObj;
         $json=json_encode($this->jsonInfor);
         self::writeJsonToConfig($json,$this->cacheUrl.'/'.$this->token);
     }
@@ -57,6 +66,22 @@ class OxygenBurst
         }
         return true;
     }
+    /*查找指定会话是否存在*/
+    public function getTokenIsHave(){
+        if(!file_exists($this->cacheUrl.'/'.$this->token)){
+            return false;
+        }
+        return true;
+    }
+    /*查找指定指纹是否存在*/
+    public function getShardFingerprint(){
+        if(isset($this->jsonInfor['nowFile']["{$this->shardFingerprint}"])){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     /*读取json信息*/
     private function readJson($fileUrl){
         $file=fopen($fileUrl, "r");
@@ -68,13 +93,14 @@ class OxygenBurst
     private function createJsonConfig(){
         /*
          * 格式: {"token":会话名,"suffix":整合后的后缀名,"total":应该上传的文件总片数,"successFileCount":上传成功的文件数,
-         * "nowFile":[文件1,文件2] 当前存储的文件列表路径}
+         * "nowFile":[{"文件指纹":"文件.rep"},{"文件指纹":"指纹.rep"}] 当前存储的文件列表路径}
          * */
         $dataTeample['token']=$this->token;
         $dataTeample['suffix']=$this->suffix;
-        $dataTeample['successFileCount']=$this->successFileCount;
         $dataTeample['nowFile']=array();
         $dataTeample['total']=$this->total;
+        $dataTeample['state']=0; //状态0.未完成1.完成
+        $this->jsonInfor=$dataTeample;
         $jsonData=json_encode($dataTeample);
         return $jsonData;
     }
@@ -94,12 +120,17 @@ class OxygenBurst
         $confArray=$this->jsonInfor;
         $baseUrl=$this->cacheUrl.'/'.$this->token;
         $fileArray=$confArray['nowFile'];
-        foreach ($fileArray as &$value){
-            $value=$baseUrl.'/'.$value;
+        $fileUrl=array();
+        foreach ($fileArray as $key=>$value){
+            $fileUrl[]=$baseUrl.'/'.$value['url'];
         }
         $oxygenUpload= new OxygenUpload($this->conformityUrl,'','file',$saveName,true);
         $oxygenUpload->mandatorySuffixName=$this->suffix;
-        $oxygenUpload->mergeFile($fileArray,$this->conformityUrl,$this->suffix);
+        $oxygenUpload->mergeFile($fileUrl,$this->conformityUrl,$this->suffix);
+        $this->jsonInfor['state']=1;
+        $json=json_encode($this->jsonInfor);
+        self::writeJsonToConfig($json,$this->cacheUrl.'/'.$this->token);
+        return true;
     }
 
 }
